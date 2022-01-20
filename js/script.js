384064c6eb2b8f71f203d7201b2c7754e9d175fc
SoyoungSheen <93234844+SoyoungSheen@users.noreply.github.com>
2022-01-20 12:57:52 +0900

    f

--- js/script.js
+++ js/script.js
@@ -87,3 +87,51 @@ $.ajax({
 
 
   });
+
+
+$.ajax({
+  method: "GET",
+  url: "https://dapi.kakao.com/v3/search/book?target=title",
+  data: { query: "FUN" },
+  headers: { Authorization: "KakaoAK b0d05791d5bd9db3627ffb7d2c90a277" }
+})
+  .done(function (msg) {
+    console.log(msg.documents[0].title);
+    console.log(msg.documents[0].thumbnail);
+
+
+    $(".book1").append("<img src='" + msg.documents[0].thumbnail + "'/>");
+    $(".book1").append("<h3>" + msg.documents[0].title + "</h3>");
+    $(".book1").append("<h6>" + msg.documents[0].authors[0] + "</h6>");
+    $(".book1").append("<p>" + msg.documents[0].price + "원" + "</p>");
+
+
+
+    $(".book2").append("<img src='" + msg.documents[1].thumbnail + "'/>");
+    $(".book2").append("<h3>" + msg.documents[1].title + "</h3>");
+    $(".book2").append("<h6>" + msg.documents[1].authors[1] + "</h6>");
+    $(".book2").append("<p>" + msg.documents[1].price + "원" + "</p>");
+
+    $(".book3").append("<img src='" + msg.documents[2].thumbnail + "'/>");
+    $(".book3").append("<h3>" + msg.documents[2].title + "</h3>");
+    $(".book3").append("<h6>" + msg.documents[2].authors[2] + "</h6>");
+    $(".book3").append("<p>" + msg.documents[2].price + "원" + "</p>");
+
+    $(".book4").append("<img src='" + msg.documents[3].thumbnail + "'/>");
+    $(".book4").append("<h3>" + msg.documents[3].title + "</h3>");
+    $(".book4").append("<h6>" + msg.documents[3].authors[3] + "</h6>");
+    $(".book4").append("<p>" + msg.documents[3].price + "원" + "</p>");
+
+    $(".book5").append("<img src='" + msg.documents[4].thumbnail + "'/>");
+    $(".book5").append("<h3>" + msg.documents[4].title + "</h3>");
+    $(".book5").append("<h6>" + msg.documents[4].authors[4] + "</h6>");
+    $(".book5").append("<p>" + msg.documents[4].price + "원" + "</p>");
+
+    $(".book6").append("<img src='" + msg.documents[5].thumbnail + "'/>");
+    $(".book6").append("<h3>" + msg.documents[5].title + "</h3>");
+    $(".book6").append("<h6>" + msg.documents[5].authors[5] + "</h6>");
+    $(".book6").append("<p>" + msg.documents[5].price + "원" + "</p>");
+
+
+
+  });
